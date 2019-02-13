650020020074f6f55f0d85458816b423288a369f
Jade <xusl_@hotmail.com>
2019-02-13 15:15:39 +0800

    format table.vue

--- src/views/table/table.vue
+++ src/views/table/table.vue
@@ -12,98 +12,97 @@
               v-model="formInline.user.date"
               align="right"
               type="year"
-              placeholder="选择年份">
-            </el-date-picker>
+              placeholder="选择年份"
+            ></el-date-picker>
           </el-form-item>
           <el-form-item label="地址">
-            <el-cascader expand-trigger="hover" :options="options" v-model="formInline.user.address"></el-cascader>
+            <el-cascader
+              expand-trigger="hover"
+              :options="options"
+              v-model="formInline.user.address"
+            ></el-cascader>
           </el-form-item>
           <el-form-item label="籍贯">
             <el-select v-model="formInline.user.place" placeholder="请选择">
-              <el-option
-                v-for="item in places"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
+              <el-option v-for="item in places" :label="item.label" :value="item.value"></el-option>
             </el-select>
           </el-form-item>
           <el-button type="primary" @click="onSubmit">查询</el-button>
-          <a href="javascript:;" id="download" style="background-color:#409EFF;color: #fff;padding: 12px 10px!important;margin-left: 10px!important;border-radius:4px " @click="download()" download="download.csv">导出数据</a>
+          <a
+            href="javascript:;"
+            id="download"
+            style="background-color:#409EFF;color: #fff;padding: 12px 10px!important;margin-left: 10px!important;border-radius:4px "
+            @click="download()"
+            download="download.csv"
+          >导出数据</a>
         </el-form>
 
         <el-container style="height: 500px; border: 1px solid #eee">
- <el-aside width="600px" style="background-color: rgb(238, 241, 246)">
-        <!--表格-->
-        <el-table
-          :data="tableData"
-           :show-header="false"
-          border
-          style="width: 100%">
-          <el-table-column type="selection">
-          </el-table-column>
-          <el-table-column
-          prop="date"
-            label="出生日期"
-            width="180">
-            <template scope="scope">
-            <img src="static/images/hamburger.png" class="image">
-            <span> {{ scope.row.date }}</span>
-            <span> {{ scope.row.address }}</span>
-            <span> {{ scope.row.company }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="姓名"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="地址">
-          </el-table-column>
-          <el-table-column label="操作">
-            <template scope="scope">
-              <el-button type="primary"  style="display: block;" size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-              <el-button type="danger" size="small" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-  </el-aside> 
+          <el-aside width="600px" style="background-color: rgb(238, 241, 246)">
+            <!--表格-->
+            <el-table :data="tableData" :show-header="false" border style="width: 100%">
+              <el-table-column type="selection"></el-table-column>
+              <el-table-column prop="date" label="出生日期" width="180">
+                <template scope="scope">
+                  <img src="static/images/hamburger.png" class="image">
+                  <span>{{ scope.row.date }}</span>
+                  <span>{{ scope.row.address }}</span>
+                  <span>{{ scope.row.company }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+              <el-table-column prop="address" label="地址"></el-table-column>
+              <el-table-column label="操作">
+                <template scope="scope">
+                  <el-button
+                    type="primary"
+                    style="display: block;"
+                    size="small"
+                    @click="handleEdit(scope.$index, scope.row)"
+                  >编辑</el-button>
+                  <el-button
+                    type="danger"
+                    size="small"
+                    @click="handleDelete(scope.$index, scope.row)"
+                  >删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-aside>
 
-<el-main class="activity-detail">
-      <img src="static/images/hamburger.png" class="image">
-            <span> {{ currentItem.name }}</span>
-            <div> {{ currentItem.company }}</div>
-            <div class="address"> {{ currentItem.address }}</div>
-            <div class="description"> {{ currentItem.description }}</div>
+          <el-main class="activity-detail">
+            <img src="static/images/hamburger.png" class="image">
+            <span>{{ currentItem.name }}</span>
+            <div>{{ currentItem.company }}</div>
+            <div class="address">{{ currentItem.address }}</div>
+            <div class="description">{{ currentItem.description }}</div>
             <div style="float:right">
-            <el-button type="text" @click="handleLike">点赞</el-button>
-            <span style="color: #409EFF;">|</span> 
-            <el-button type="text" icon="el-icon-edit" @click="handleComment">评论</el-button>
+              <el-button type="text" @click="handleLike">点赞</el-button>
+              <span style="color: #409EFF;">|</span>
+              <el-button type="text" icon="el-icon-edit" @click="handleComment">评论</el-button>
             </div>
             <el-input v-model="commentInput" type="textarea" autosize placeholder="请输入内容"></el-input>
-</el-main>
+          </el-main>
 
-  <el-aside> 
-    <span class="title">基本信息</span>
- <el-form label-position="top" size="medium" inline class="demo-table-expand">
-          <el-form-item label="客户名称">
-            <span>123</span>
-          </el-form-item>          
-        </el-form>
+          <el-aside>
+            <span class="title">基本信息</span>
+            <el-form label-position="top" size="medium" inline class="demo-table-expand">
+              <el-form-item label="客户名称">
+                <span>123</span>
+              </el-form-item>
+            </el-form>
 
-    <span class="title">其他信息</span>
- <el-form label-position="top" inline  size="mini" class="demo-table-expand">
-          <el-form-item label="拜访时间">
-            <span>2019.1.20 09:10</span>
-          </el-form-item>
-          <el-form-item label="提交时间">
-            <span>2019.1.20 18:30</span>
-          </el-form-item>         
-        </el-form>
-  </el-aside> 
-
-  </el-container>
+            <span class="title">其他信息</span>
+            <el-form label-position="top" inline size="mini" class="demo-table-expand">
+              <el-form-item label="拜访时间">
+                <span>2019.1.20 09:10</span>
+              </el-form-item>
+              <el-form-item label="提交时间">
+                <span>2019.1.20 18:30</span>
+              </el-form-item>
+            </el-form>
+          </el-aside>
+        </el-container>
 
         <div class="block">
           <el-pagination
@@ -112,8 +111,8 @@
             :current-page="currentPage"
             :page-size="100"
             layout="prev, pager, next, jumper"
-            :total="1000">
-          </el-pagination>
+            :total="1000"
+          ></el-pagination>
         </div>
       </el-col>
     </el-row>
@@ -126,7 +125,7 @@
           <el-input v-model="form.address"></el-input>
         </el-form-item>
         <el-form-item label="出生日期">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;" ></el-date-picker>
+          <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;"></el-date-picker>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="handleSave" :loading="editLoading">修改</el-button>
@@ -137,197 +136,198 @@
   </section>
 </template>
 <script type="text/ecmascript-6">
-  const ERR_OK = "000";
-  export default {
-    data () {
-      return {
-        formInline: {
-          user: {
-            name: '',
-            date: '',
-            address: [],
-            place: ''
-          }
-        },
-        tableData: [],
-        options: [],
-        places: [],
-        dialogFormVisible: false,
-        editLoading: false,
-        form: {
-          name: '',
-          address: '',
-          date: '',
-        },
-        currentPage: 1,
-        table_index: 0,
-        commentInput: ""
-      };
-    },
-    created () {
-      this.$http.get('/api/getTable').then((response) => {
-        response = response.data;
-        if (response.code === ERR_OK) {
-          this.tableData = response.datas;
+const ERR_OK = "000";
+export default {
+  data() {
+    return {
+      formInline: {
+        user: {
+          name: "",
+          date: "",
+          address: [],
+          place: ""
         }
-      });
-      this.$http.get('/api/getOptions').then((response) => {
-        response = response.data;
-        if (response.code === ERR_OK) {
-          this.options = response.datas;
-          this.places = response.places;
-        }
-      });
-    },
-    computed: {
-      // a computed getter
-      currentItem: function () {
-        // `this` points to the vm instance
-        if (!this.tableData) {
-          return {
-            "name": "",
-            "company": "",
-            "address": ""
-          };
-        }
-        if (this.table_index >= this.tableData.length || this.table_index < 0) {
-          this.table_index = 0;
-        }
-        return this.tableData[this.table_index];
+      },
+      tableData: [],
+      options: [],
+      places: [],
+      dialogFormVisible: false,
+      editLoading: false,
+      form: {
+        name: "",
+        address: "",
+        date: ""
+      },
+      currentPage: 1,
+      table_index: 0,
+      commentInput: ""
+    };
+  },
+  created() {
+    this.$http.get("/api/getTable").then(response => {
+      response = response.data;
+      if (response.code === ERR_OK) {
+        this.tableData = response.datas;
       }
+    });
+    this.$http.get("/api/getOptions").then(response => {
+      response = response.data;
+      if (response.code === ERR_OK) {
+        this.options = response.datas;
+        this.places = response.places;
+      }
+    });
+  },
+  computed: {
+    // a computed getter
+    currentItem: function() {
+      // `this` points to the vm instance
+      if (!this.tableData) {
+        return {
+          name: "",
+          company: "",
+          address: ""
+        };
+      }
+      if (this.table_index >= this.tableData.length || this.table_index < 0) {
+        this.table_index = 0;
+      }
+      return this.tableData[this.table_index];
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message("模拟数据，这个方法并不管用哦~");
     },
-    methods: {
-      onSubmit () {
-        this.$message('模拟数据，这个方法并不管用哦~');
-      },
-      handleDelete (index, row) {
-        this.tableData.splice(index, 1);
-        this.$message({
-          message: "操作成功！",
-          type: 'success'
-        });
-      },
-      handleEdit (index, row) {
-        this.dialogFormVisible = true;
-        this.form = Object.assign({}, row);
-        this.table_index = index;
-      },
-      handleComment() {
-      },
-      handleLike() {
-      },
-      handleSave () {
-        this.$confirm('确认提交吗？', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          cancelButtonClass: 'cancel'
-        }).then(() => {
+    handleDelete(index, row) {
+      this.tableData.splice(index, 1);
+      this.$message({
+        message: "操作成功！",
+        type: "success"
+      });
+    },
+    handleEdit(index, row) {
+      this.dialogFormVisible = true;
+      this.form = Object.assign({}, row);
+      this.table_index = index;
+    },
+    handleComment() {},
+    handleLike() {},
+    handleSave() {
+      this.$confirm("确认提交吗？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        cancelButtonClass: "cancel"
+      })
+        .then(() => {
           this.editLoading = true;
           let date = this.form.date;
           if (typeof date === "object") {
-            date = [date.getFullYear(), (date.getMonth() + 1), (date.getDate())].join('-');
-            this.form.date = date
+            date = [
+              date.getFullYear(),
+              date.getMonth() + 1,
+              date.getDate()
+            ].join("-");
+            this.form.date = date;
           }
-//          this.tableData[this.table_index] = this.form;
+          //          this.tableData[this.table_index] = this.form;
           this.tableData.splice(this.table_index, 1, this.form);
           this.$message({
             message: "操作成功！",
-            type: 'success'
+            type: "success"
           });
           this.editLoading = false;
           this.dialogFormVisible = false;
-        }).catch(() => {
-
-        });
-      },
-      download: function() {
-        console.log("xiazai")
-        var obj = document.getElementById('download');
-        var str = "姓名,出生日期,地址\n";
-        for (var i = 0; i < this.tableData.length; i++) {
-          var item = this.tableData[i];
-          str += item.name + ',' + item.date + ',' + item.address;
-          str += "\n";
-        }
-        console.log(obj)
-        str = encodeURIComponent(str);
-        console.log(str)
-        obj.href = "data:text/csv;charset=utf-8,\ufeff" + str;
-        obj.download = "download.csv";
-      },
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-      },
-      handleCurrentChange(val) {
-        this.currentPage = val;
-        console.log(`当前页: ${val}`);
+        })
+        .catch(() => {});
+    },
+    download: function() {
+      console.log("xiazai");
+      var obj = document.getElementById("download");
+      var str = "姓名,出生日期,地址\n";
+      for (var i = 0; i < this.tableData.length; i++) {
+        var item = this.tableData[i];
+        str += item.name + "," + item.date + "," + item.address;
+        str += "\n";
       }
+      console.log(obj);
+      str = encodeURIComponent(str);
+      console.log(str);
+      obj.href = "data:text/csv;charset=utf-8,\ufeff" + str;
+      obj.download = "download.csv";
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      console.log(`当前页: ${val}`);
     }
-  };
+  }
+};
 </script>
 <style>
-  .el-pagination {
-    text-align: center;
-    margin-top: 30px;
-  }
-  .el-message-box__btns .cancel {
-    float: right;
-    margin-left: 10px;
-  }
+.el-pagination {
+  text-align: center;
+  margin-top: 30px;
+}
+.el-message-box__btns .cancel {
+  float: right;
+  margin-left: 10px;
+}
 
-    .demo-table-expand {
-    font-size: 0;
-  }
-  .demo-table-expand label {
-    /* width: 90px; */
-    color: #99a9bf;
-  }
-  .demo-table-expand .el-form-item {
-    display: block;
-    margin-right: 0;
-    margin-bottom: 0;
-    /* width: 50%; */
-  }
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  /* width: 90px; */
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  display: block;
+  margin-right: 0;
+  margin-bottom: 0;
+  /* width: 50%; */
+}
 
-  .title {
-    font-size: large;
-    font-weight: bold; 
-    padding:  20px 0 20px;
-    line-height: 60px;
-  }
+.title {
+  font-size: large;
+  font-weight: bold;
+  padding: 20px 0 20px;
+  line-height: 60px;
+}
 
-  .image {
-    width: 60px;
-    height: 60px;
-    display: block;
-  }
+.image {
+  width: 60px;
+  height: 60px;
+  display: block;
+}
 
 .activity-detail {
-
 }
 
-  .activity-detail .avatar {
-    width: 80px;
-    height: 80px;
-    padding: 3px;
-  }
+.activity-detail .avatar {
+  width: 80px;
+  height: 80px;
+  padding: 3px;
+}
 
-  .activity-detail .name {
-    font-size: medium;
-  }
+.activity-detail .name {
+  font-size: medium;
+}
 
-  .activity-detail .contact-info {
-    color: lightskyblue;
-  }
+.activity-detail .contact-info {
+  color: lightskyblue;
+}
 
 .address {
-    padding:  10px 0 10px;
+  padding: 10px 0 10px;
 }
-  .address:before{
-    content: "地址："
-  }
+.address:before {
+  content: "地址：";
+}
 
-  .description {
-    padding:  10px 0 10px;
-  }
+.description {
+  padding: 10px 0 10px;
+}
 </style>
